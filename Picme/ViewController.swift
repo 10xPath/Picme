9e3a9256778f1cb8c63d9cf134850f38277f025c
John Nguyen <htjohn@Johns-MacBook-Pro.local>
2015-04-04 22:28:41 -0700

    Initial Commit

---
+++ Picme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Picme
+//
+//  Created by John Nguyen on 4/4/15.
+//  Copyright (c) 2015 John Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
